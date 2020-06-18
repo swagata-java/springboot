614e60d67cc7c78bc3fa9a64a0e223399dd7ac57
swagata-java <67113104+swagata-java@users.noreply.github.com>
2020-06-18 14:36:31 -0400

    Add files via upload
    Resource Server

---
+++ SpringBootFormHandingApplication.java
@@ -0,0 +1,13 @@
+package com.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormHandingApplication {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(SpringBootFormHandingApplication.class, args);
+	}
+
+}
